f7d809f06c12ca0b56564d916e8f6a218fec2746
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2025-07-07 14:25:54 +0700

    Create hello_uname.sh

---
+++ pertemuan-01-intro-shell/hello_uname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Program Pertemuan 1
+## pertemuan-01-intro-shell/hello_uname.sh
+# Menampilkan ucapan dan informasi sistem dasar
+
+echo "Selamat datang di dunia Linux!"
+echo "Tanggal saat ini: $(date)"
+echo "Nama pengguna: $USER"
+echo "Sistem: $(uname -a)"
